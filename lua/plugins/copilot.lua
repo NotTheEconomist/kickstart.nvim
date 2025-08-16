7a01374568d0085a1d7fe5e5ca08bbc58cc7596c
Adam Smith <adamsmith@nottheeconomist.com>
2025-08-16 12:09:57 -0700

    fix: copilot config
    Also: add luarc for lua_ls

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,9 +2,10 @@ return {
     "github/copilot.vim",
     config = function()
         vim.g.copilot_no_tab_map = true
-        vim.g.copilot_assume_mapped = true
-        vim.g.enable_copilot = false
-        vim.keymap.set("i", "<C-y>", "<Plug>(copilot-accept)", { silent = true, desc = "Accept Copilot suggestion" })
+        vim.g.copilot_filetypes = {
+            ["*"] = false,
+        }
+        vim.keymap.set("i", "<C-y>", "copilot#Accept('\\<CR>')", { silent = true, expr=true, replace_keycodes=false, desc = "Accept Copilot suggestion" })
         vim.keymap.set("i", "<C-n>", "<Plug>(copilot-dismiss)", { silent = true, desc = "Dismiss Copilot suggestion" })
     end
 }
