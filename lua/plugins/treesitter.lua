c995eaf360a708df26b9c846c3f12099b39899e4
Adam Smith <asmithpipeworks.com@USPWSM22779.local>
2026-05-20 16:39:45 -0700

    fix: telescope and treesitter updates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-        require 'nvim-treesitter.configs'.setup({
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = { enable = true },
-
-            highlight = {
-                enable = true,
-            },
-        })
+        require('nvim-treesitter').install { "c", "lua", "vim", "vimdoc", "query" }
     end
 }
